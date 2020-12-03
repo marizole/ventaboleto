2676e72ff3e8741d39109f2cf8efc9f523c42a37
unknown <ninaram1408@gmail.com>
2020-12-03 12:09:36 -0400

    commit

--- messageexec.php
+++ messageexec.php
@@ -8,7 +8,7 @@
 	<br>
 <br>
 	<center><img src="img/21.jpg" width="400" height="300"></center>
-	<CENTER><h1><FONT color="red">SU MENSAJE FUE RECIBIDO</FONT></h1>
+	<CENTER><h1><FONT color="red">SU MENSAJE FUE RECIBIDO CORRECTAMENTE</FONT></h1>
 	<H1>EXITOSAMENTE !!!</H1></CENTER>
 
 </body>
