38be996dadb3a32afe2025f5eb9dbbcd810b6b67
unknown <ninaram1408@gmail.com>
2020-12-04 19:08:35 -0400

    conexion DB

---
+++ conexionDB.php
@@ -0,0 +1,14 @@
+<?php
+$mysql_hostname = "localhost";
+$mysql_user = "root";
+$mysql_password = "";
+$mysql_database = "db_ventas";
+$prefix = "";
+
+
+$db = new mysqli ($mysql_hostname, $mysql_user, $mysql_password);
+if($db->connect_error > 0){
+    die('ERROR DE CONEXION [' . $db->connect_error. ']');
+}
+
+?>
